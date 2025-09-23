2af6e10a99120473f13f1da0f1fc243d03ef0da3
VadimGlazkow <74980497+VadimGlazkow@users.noreply.github.com>
2025-09-23 16:15:48 +0000

    Добавлены файлы к лабе 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include "string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str);
+	char tmp;
+	
+	for (int i = 0; i < length / 2; i++) 
+	{
+		tmp = str[i];
+		str[i] = str[length - i - 1];
+		str[length - i - 1] = tmp;
+	}
 }
 
